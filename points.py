60f3f8db6ca4279733e71ef36ed6d72cc14d0f88
Nik Everdeen <nikita.kigay@gmail.com>
2020-05-07 13:43:48 +0600

    version1

--- points.py
+++ points.py
@@ -1,13 +1,17 @@
 import requests, distance
 
 url = 'https://geocode-maps.yandex.ru/1.x/?apikey=46baa895-6d77-4b6b-8fe6-a88c43a5f388&format=json&geocode='
-school_adress = input('Адрес первой точки: ')
-home_adress = input('Адрес второй точки: ')
-s_resp = requests.get(url + school_adress)
+
+print('Расстояние от двух точек.')
+
+first_adress = input('Адрес первой точки: ')
+second_adress = input('Адрес второй точки: ')
+
+s_resp = requests.get(url + first_adress)
 s_point = s_resp.json()['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos'].split(' ')
 s_point[0], s_point[1] = float(s_point[0]), float(s_point[1])
 
-h_resp = requests.get(url + home_adress)
+h_resp = requests.get(url + second_adress)
 h_point = h_resp.json()['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos'].split(' ')
 h_point[0], h_point[1] = float(h_point[0]), float(h_point[1])
 
